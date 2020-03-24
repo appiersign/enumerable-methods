62921cca8725568c03b05df190526963a84af7d9
Solomon APPIER-SIGN <braasig@gmail.com>
2020-03-24 19:21:34 +0000

    my_each_with_index method added

--- main.rb
+++ main.rb
@@ -4,6 +4,13 @@ module Enumerable
       yield(x)
     end
   end
+
+  def self.my_each_with_index(array)
+    for x in (0...array.length) do
+      yield(x, array[x])
+    end
+  end
 end
 
-Enumerable.my_each([1, 2, 4]) { |a| puts a }
+# Enumerable.my_each([1, 2, 4]) { |a| puts a }
+Enumerable.my_each_with_index([1, 2, 4]) { |a, b| puts(a, b) }
