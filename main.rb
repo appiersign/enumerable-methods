a122953d1bceed9718019a9a49dd4acb6bb9d95d
Solomon APPIER-SIGN <braasig@gmail.com>
2020-03-26 21:33:52 +0000

    my_each, my_each_with_index, my_select and my_all? tested

--- main.rb
+++ main.rb
@@ -1,37 +1,57 @@
 module Enumerable
   def my_each
-    if self.class == Range
-      arr = to_a
-      arr.length.times { |x| yield(arr[x]) }
-    elsif self.class == Hash
-      hash_keys = keys
-      length.times { |x| yield(self[hash_keys[x]]) }
+    if block_given?
+      if self.class == Range
+        arr = to_a
+        arr.length.times { |x| yield(arr[x]) }
+      elsif self.class == Hash
+        hash_keys = keys
+        length.times { |x| yield(self[hash_keys[x]]) }
+      else
+        length.times { |x| yield(self[x]) }
+      end
+      self
     else
-      length.times { |x| yield(self[x]) }
+      to_enum(:my_each)
     end
   end
 
   def my_each_with_index
-    if is_a? Range
-      arr = to_a
-      arr.length.times { |x| yield(x, arr[x]) }
-    elsif self.class == Hash
-      hash_keys = keys
-      length.times { |x| yield(hash_keys[x], self[hash_keys[x]]) }
+    if block_given?
+      if is_a? Range
+        arr = to_a
+        arr.length.times { |x| yield(x, arr[x]) }
+      elsif self.class == Hash
+        hash_keys = keys
+        length.times { |x| yield(hash_keys[x], self[hash_keys[x]]) }
+      else
+        length.times { |x| yield(x, self[x]) }
+      end
+      self
     else
-      length.times { |x| yield(x, self[x]) }
+      to_enum(:my_each_with_index)
     end
   end
 
   def my_select
-    array = []
-    my_each { |x| array << x if yield(x) }
-    array
+    if block_given?
+      array = []
+      my_each { |x| array << x if yield(x) }
+      array
+    else
+      to_enum(:my_select)
+    end
   end
 
-  def my_all?
+  def my_all?(arg = nil)
     array = []
-    my_select { |x| array << x if yield(x) }
+    if arg
+      my_select { |x| array << x if x.to_s.match(arg) and x.is_a? nil == false }
+    elsif block_given?
+      my_select { |x| array << x if yield(x) and x.is_a? nil == false }
+    else
+      my_select { |x| array << x }
+    end
     array.length == length
   end
 
@@ -43,10 +63,14 @@ module Enumerable
     true
   end
 
-  def my_none?
+  def my_none?(args = nil)
     array = []
-    my_select { |x| array << x unless yield(x) }
-    array.length == length
+    if args.nil?
+      my_select { |x| array << x unless yield(x) }
+    else
+      my_select { |x| array << x if x.match(args) }
+    end
+    array.empty?
   end
 
   def my_count
@@ -68,3 +92,7 @@ module Enumerable
     array.my_inject(1) { |p, x| p * x }
   end
 end
+
+print [1, 2, 3].my_all?(/d/)
+puts
+print [1, 2, 3].all?(/d/)
